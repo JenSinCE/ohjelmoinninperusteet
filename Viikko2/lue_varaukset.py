ecab9b090e91f8654b49acdbe9f472b6231e9fc1
Jenni Sinisaari <jenni.sinisaari@centria.fi>
2025-11-16 19:32:35 +0200

    toimivaohjelma

--- Viikko2/lue_varaukset.py
+++ Viikko2/lue_varaukset.py
@@ -16,29 +16,40 @@ Sähköposti: anna.virtanen@example.com
 
 """
 
+from datetime import datetime
+
 def main():
-    
-    varaukset = "varaukset.txt"
+    tiedosto = "varaukset.txt"
 
-    
-    with open(varaukset, "r", encoding="utf-8") as f:
-        varaus = f.read().strip()
+    with open(tiedosto, "r", encoding="utf-8") as f:
+        rivit = f.readlines()
 
-    
-    print(varaus)
+    for rivi in rivit:
+        varaus = rivi.strip().split('|')
+        varausnumero = varaus[0]
+        varaaja = varaus[1]
+        paiva = varaus[2]
+        aika = varaus[3]
+        tuntimaara = varaus[4]
+        tuntihinta = float(varaus[5].replace(",", "."))
+        kokonaishinta = float(tuntimaara) * tuntihinta
+        maksettu = varaus[6]
+        kohde = varaus[7]
+        puhelin = varaus[8]
+        sahkoposti = varaus[9]
 
-    # Kokeile näitä
-    #print(varaus.split('|'))
-    #varausId = varaus.split('|')[0]
-    #print(varausId)
-    #print(type(varausId))
-    """
-    Edellisen olisi pitänyt tulostaa numeron 123, joka
-    on oletuksena tekstiä.
+        print(f"Varausnumero: {varausnumero}")
+        print(f"Varaaja: {varaaja}")
+        print(f"Päivämäärä: {paiva}")
+        print(f"Aloitusaika: {aika}")
+        print(f"Tuntimäärä: {tuntimaara}")
+        print(f"Tuntihinta: {tuntihinta} €")
+        print(f"Kokonaishinta: {kokonaishinta} €")
+        print(f"Maksettu: {maksettu}")
+        print(f"Kohde: {kohde}")
+        print(f"Puhelin: {puhelin}")
+        print(f"Sähköposti: {sahkoposti}")
 
-    Voit kokeilla myös vaihtaa kohdan [0] esim. seuraavaksi [1]
-    ja testata mikä muuttuu
-    """
 
 if __name__ == "__main__":
     main()
